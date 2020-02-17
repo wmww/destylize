7c625fae22b3d4a53f7d6cadd4db6550a184e974
William Wold <wm@wmww.sh>
2020-02-17 16:58:18 -0500

    Move functionality into new scanner class

--- src/destylize.js
+++ src/destylize.js
@@ -1,58 +1,13 @@
-function fix_text(text) {
-    replacements = []
-    for (let c of text) {
-        if (a = char_map.get(c)) {
-            replacements.push([c, a]);
-        }
-    }
-    for (let r of replacements) {
-        text = text.replace(r[0], r[1]);
-    }
-    return text;
-}
-
-function scan_node(node) {
-    if (node.nodeType == Node.TEXT_NODE) {
-        // Don't replace in text entry areas
-        if (!node.parentNode || node.parentNode.nodeName != "TEXTAREA") {
-            node.textContent = fix_text(node.textContent);
-        }
-    } else {
-        node.childNodes.forEach(scan_node);
-    }
-}
-
-function enable() {
-    console.log("Enabled Destylize!");
-
-    scan_node(document.body);
-
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach((m) => {
-            if (m.type == "characterData") {
-                scan_node(m.target);
-            }
-            if (m.addedNodes) {
-                for (let node of m.addedNodes) {
-                    scan_node(node);
-                }
-            }
-        });
-    });
-
-    observer.observe(document.body, {childList: true, subtree: true});
-}
-
-function disable() {
-    console.log("Disabled Destylize");
-}
+const scanner = new Scanner();
 
 function set_enabled(enabled) {
     if (enabled) {
-        enable();
+        scanner.enable();
+        console.log("Destylize enbled!");
     }
     else {
-        disable();
+        scanner.disable();
+        console.log("Destylize disabled");
     }
 }
 
@@ -70,3 +25,4 @@ function local_storage_change(changes, area) {
 
 browser.storage.onChanged.addListener(local_storage_change);
 browser.storage.local.get("enabled", value => set_enabled(value.enabled));
+console.log("Destylize initialized");
---
+++ src/scanner.js
@@ -0,0 +1,49 @@
+function fix_text(text) {
+    replacements = []
+    for (let c of text) {
+        if (a = char_map.get(c)) {
+            replacements.push([c, a]);
+        }
+    }
+    for (let r of replacements) {
+        text = text.replace(r[0], r[1]);
+    }
+    return text;
+}
+
+function scan_node(node) {
+    if (node.nodeType == Node.TEXT_NODE) {
+        // Don't replace in text entry areas
+        if (!node.parentNode || node.parentNode.nodeName != "TEXTAREA") {
+            node.textContent = fix_text(node.textContent);
+        }
+    } else {
+        node.childNodes.forEach(scan_node);
+    }
+}
+
+class Scanner {
+    constructor() {
+        this.observer = new MutationObserver((mutations) => {
+            mutations.forEach((m) => {
+                if (m.type == "characterData") {
+                    scan_node(m.target);
+                }
+                if (m.addedNodes) {
+                    for (let node of m.addedNodes) {
+                        scan_node(node);
+                    }
+                }
+            });
+        });
+    }
+
+    enable() {
+        // initial scan
+        scan_node(document.body);
+        this.observer.observe(document.body, {childList: true, subtree: true});
+    }
+
+    disable() {
+    }
+}
