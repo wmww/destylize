7583e5e2d7434e224de005fb9b2c4edf9beb883f
William Wold <wm@wmww.sh>
2020-02-21 00:16:12 -0500

    Add package script

---
+++ scripts/package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Packages the extension into a .zip folder, only tested on Linux
+
+if test ! -f "manifest.json"; then
+    echo "Please run from the root project directory"
+fi
+
+ZIP_DEST='./data/destylize.zip'
+
+rm -f "$ZIP_DEST"
+
+# Screw it it works
+find . \
+    -not -path './.git*' \
+    -not -path './data*' \
+    -not -path './test*' \
+    -not -path './scripts*' \
+    -not -name 'destylize-400.png' \
+    -not -name 'example_text.png' \
+    -not -name '*.kra' \
+    -not -name '*~' \
+    | xargs zip "$ZIP_DEST"
