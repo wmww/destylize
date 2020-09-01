017f6db1d03f1e7636f7e0c041ed0d2f9dafb1e9
William Wold <wm@wmww.sh>
2020-08-31 23:34:07 -0700

    Minor improvements to packing script

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,13 +2,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Packages the extension into a .zip folder, only tested on Linux
+echo "This script packages the extension into a .zip folder, only tested on Linux"
 
 if test ! -f "manifest.json"; then
     echo "Please run from the root project directory"
+    exit 1
 fi
 
 ZIP_DEST='./data/destylize.zip'
+echo "$ZIP_DEST will be created/overwitten"
+echo "[Enter]: Continue, [Ctrl]+[C]: Quit"
+read
 
 rm -f "$ZIP_DEST"
 
@@ -25,4 +29,7 @@ find . \
     -not -name '*.kra' \
     -not -name '*~' \
     -not -path './store_description.md' \
+    -not -path './release-process.md' \
     | xargs zip "$ZIP_DEST"
+
+echo "Please read the above list of included files. If any should not be there add them to the ignore list in this script and re-run"
